e62afd344ff78fed9c0fbf98335df2e5becc9a03
DavisDavisDavis <olizoooom@gmail.com>
2022-06-08 21:49:16 +0200

    Initial

---
+++ sushi_api/schemas/product.js
@@ -0,0 +1,32 @@
+export default {
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    // {
+    //   name: "slug",
+    //   title: "Slug",
+    //   type: "slug",
+    //   options: {
+    //     source: "title",
+    //   },
+    // },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      media: "image",
+    },
+  },
+};
